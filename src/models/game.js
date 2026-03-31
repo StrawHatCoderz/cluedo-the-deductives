f0f8d6586269d224b7fb1b5d49f8c764a365a7d4
Javed Ahmed <javedahmed@Javeds-MacBook-Pro.local>
2026-03-31 17:34:37 +0530

    [#7] | changes the setTurnOrder method of Game from public to private | Rahul/Javed

--- src/models/game.js
+++ src/models/game.js
@@ -25,6 +25,7 @@ export class Game {
   start() {
     this.#distributeCards();
     this.changeCurrentState();
+    this.setTurnOrder();
   }
 
   getCurrentState() {
@@ -40,7 +41,7 @@ export class Game {
   }
 
   setTurnOrder() {
-    this.#turnOrder = this.#shuffle(this.#players);
+    this.#turnOrder = this.#shuffle(Object.values(this.#players));
   }
 
   addPlayer(player) {
